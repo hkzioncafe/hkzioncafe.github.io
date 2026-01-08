8f157a5bd30b345c9000c084bdd3289562368a80
hkzioncafe <hkzioncafe@gmail.com>
2026-01-08 23:26:52 +0800

    Add files via upload

---
+++ js/const.js
@@ -0,0 +1,12 @@
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbyLjNQIA5wnXWxh1w2uMUukxv0O87uQpSHbVHt_VI3leSNvaZLRjb0NZBZjtUwyZQWFNA/exec';
+const YOUR_CLIENT_ID = '876557140858-2mc16osg1aojod8jqvgsa35mvck0a55p.apps.googleusercontent.com';
+const YOUR_REDIRECT_URI = 'https://visitorbookhk.github.io/register';
+const lang_op = {
+  'en': 'English',
+  'zh': '繁體中文',
+  'ko': '한국어',
+};
+const alertModal = new bootstrap.Modal(document.getElementById('alertModal'), {backdrop: 'static', keyboard: false});
+const inputModal = new bootstrap.Modal(document.getElementById('inputModal'), {backdrop: 'static', keyboard: false});
+const confirmModal = new bootstrap.Modal(document.getElementById('confirmModal'), {backdrop: 'static', keyboard: false});
+const scanModal = new bootstrap.Modal(document.getElementById('scanModal'), {backdrop: 'static', keyboard: false});
