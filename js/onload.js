075402fc20b8b485e3aa6ec7db74b87f3fd43c3d
hkzioncafe <hkzioncafe@gmail.com>
2026-01-10 13:01:47 +0800

    Add files via upload

--- js/onload.js
+++ js/onload.js
@@ -90,3 +90,45 @@ function gasSubmitReg() {
 $(document).ready(function() {
   createLangView();
 });
+
+
+
+$(document).ready(function() {
+  gid = localStorage.getItem('gid');
+  // login
+  var access_token = '';
+  // Parse query string to see if page request is coming from OAuth 2.0 server.
+  var fragmentString = location.hash.substring(1);
+  var params = {};
+  var regex = /([^&=]+)=([^&]*)/g, m;
+  while (m = regex.exec(fragmentString)) {
+    params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+  }
+  if (Object.keys(params).length > 0 && params['state'] && params['access_token']) {
+    access_token = params['access_token'];
+  }else{
+    access_token = localStorage.getItem('access_token');
+  }
+  if (access_token !== null) {
+    on();
+    var url = GAS_URL+'?action=login&token='+access_token;
+    $.getJSON(url, function(data) {
+      if (data !== null) {
+        if (data.status=='0') {
+          window.history.pushState({}, document.title, "?");
+          localStorage.setItem('userinfo', JSON.stringify(data.res));
+          localStorage.setItem('access_token', access_token);
+          console.log(JSON.stringify(data.res));
+          createMainView()
+          off();
+        }else{
+          alert('已過期，請重新登入');
+          logout();
+        }
+      }
+    });
+  }else{
+    off();
+    createGLoginView();
+  }
+});
