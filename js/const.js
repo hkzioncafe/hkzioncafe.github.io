e87def90636c0dbdd25912090e48bbc6810be96f
hkzioncafe <hkzioncafe@gmail.com>
2026-01-14 02:47:21 +0800

    Add files via upload

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycby_TcG0WGfjwezE3xPh-D9GdApeuD4FrVQJn6iEFyFxfuMD0O3Nj3hVsM6_5UoXNyYU/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycby9lkh_dJpdAG6OwRoHIhM0Vga29J4UbHGTIoldKeHf5xiU2vhMImwvrJQt40up7lui/exec';
 const YOUR_CLIENT_ID = '416770209134-tjtguj3gstg24fvvrtrj890p533bsuim.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://hkzioncafe.github.io';
 const lang_op = {
