f29f0f3663d689e87395c0c8203a515bcc4d5370
hkzioncafe <hkzioncafe@gmail.com>
2026-04-09 00:51:27 +0800

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbx6txuNexfo3pvUF6MkVO1_IkXLs6ZpsaKMmXczezK0x6GRBiEehwBuSMZYC68yeiEB/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwYEPo9nvDOpvWEgdGH7TzSIUdYDujAq3D5KFEYaM2XMyZnvbnnXxkKadYiU0O19DaO/exec';
 const YOUR_CLIENT_ID = '416770209134-tjtguj3gstg24fvvrtrj890p533bsuim.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://hkzioncafe.github.io';
 const lang_op = {
